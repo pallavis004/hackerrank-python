cee29f18cae600551146e5e245f4de1cd79876d0
pallavis004 <pallavisnair004@gmail.com>
2026-07-01 21:57:51 +0530

    Positive/Negative/Zero ratios

---
+++ minus.py
@@ -0,0 +1,21 @@
+def plusMinus(arr):
+    n = len(arr)
+    positive = 0
+    negative = 0
+    zero = 0
+    
+    for x in arr:
+        if x > 0:
+            positive += 1
+        elif x < 0:
+            negative += 1
+        else:
+            zero += 1
+    
+    print(format(positive/n, '.6f'))
+    print(format(negative/n, '.6f'))
+    print(format(zero/n, '.6f'))
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    plusMinus(arr)
