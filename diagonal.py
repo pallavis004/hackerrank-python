47192d3a7686ce5186d6a5106fc8500f69131328
pallavis004 <pallavisnair004@gmail.com>
2026-07-01 21:23:54 +0530

    absolute difference of the diagonal sums

---
+++ diagonal.py
@@ -0,0 +1,20 @@
+def diagonalDifference(arr):
+    n = len(arr)
+    left_to_right = 0
+    right_to_left = 0
+    
+    for i in range(n):
+        left_to_right += arr[i][i]
+        right_to_left += arr[i][n-i-1]
+    
+    return abs(left_to_right - right_to_left)
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+    print(result)
