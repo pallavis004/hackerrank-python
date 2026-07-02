74319040fa7967f7b9342fc0d23394a48c7d8a01
pallavis004 <pallavisnair004@gmail.com>
2026-07-02 20:37:47 +0530

    flipping the matrix

---
+++ flip.py
@@ -0,0 +1,20 @@
+def flippingMatrix(matrix):
+    n = len(matrix) // 2
+    total = 0
+    for i in range(n):
+        for j in range(n):
+            total += max(
+                matrix[i][j],
+                matrix[i][2*n-1-j],
+                matrix[2*n-1-i][j],
+                matrix[2*n-1-i][2*n-1-j]
+            )
+    return total
+
+
+if __name__ == '__main__':
+    q = int(input().strip())
+    for _ in range(q):
+        n = int(input().strip())
+        matrix = [list(map(int, input().rstrip().split())) for _ in range(2 * n)]
+        print(flippingMatrix(matrix))
