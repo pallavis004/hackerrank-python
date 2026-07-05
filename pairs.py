4c22f7d8b4b0882f351b025ef68914829a1b09e4
pallavis004 <pallavisnair004@gmail.com>
2026-07-05 13:24:14 +0530

    divisible sum pairs

---
+++ pairs.py
@@ -0,0 +1,11 @@
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+    return count
+
+
+if __name__ == "__main__":
+    print(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2])) 
