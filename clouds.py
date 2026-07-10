ee5daf2d57549ce83d767147fa0b10cc12d7c428
pallavis004 <pallavisnair004@gmail.com>
2026-07-10 20:23:17 +0530

    jumping on clouds

---
+++ clouds.py
@@ -0,0 +1,14 @@
+def jumpingOnClouds(c, k):
+    n = len(c)
+    e, i = 100, 0
+    while True:
+        i = (i + k) % n
+        e -= 1 + 2 * c[i]
+        if i == 0:
+            break
+    return e
+
+if __name__ == '__main__':
+    n, k = map(int, input().rstrip().split())
+    c = list(map(int, input().rstrip().split()))
+    print(jumpingOnClouds(c, k))
