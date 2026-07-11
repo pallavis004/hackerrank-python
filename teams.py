aba9deafa362a9559c4ccfd57be8b923c23f2291
pallavis004 <pallavisnair004@gmail.com>
2026-07-11 23:06:35 +0530

    ACM ICPC teams

---
+++ teams.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+def acmTeam(topic):
+    counts = [bin(int(a, 2) | int(b, 2)).count('1') for a, b in combinations(topic, 2)]
+    m = max(counts)
+    return [m, counts.count(m)]
+if __name__ == '__main__':
+    n, m = map(int, input().split())  # n = number of people, m = number of topics
+    topic = []
+    for _ in range(n):
+        topic.append(input().strip())
+    result = acmTeam(topic)
+    print(result[0])
+    print(result[1])
